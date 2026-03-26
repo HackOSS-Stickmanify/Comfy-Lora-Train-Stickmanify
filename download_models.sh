4000457373a177aeea024f4a32a0e09555209b67
ryash072007 <ryash072007@gmail.com>
2026-03-26 09:59:23 +0000

    Update .gitignore and download script to include huggingface.co and ai-toolkit paths

--- download_models.sh
+++ download_models.sh
@@ -2,3 +2,4 @@ wget https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/sp
 wget https://huggingface.co/Kijai/WanVideo_comfy/resolve/main/Wan21_CausVid_bidirect2_T2V_1_3B_lora_rank32.safetensors -P /root/comfy/ComfyUI/models/loras
 wget https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/vae/wan_2.1_vae.safetensors -p /root/comfy/ComfyUI/models/vae
 wget https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/text_encoders/umt5_xxl_fp16.safetensors -P /root/comfy/ComfyUI/models/text_encoders
+cp -a workflows/. /root/comfy/ComfyUI/user/default/workflows/
